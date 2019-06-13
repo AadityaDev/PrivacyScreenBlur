6810d34002f741078bda43ce65d60af6a0f3a3e6
Manish Kumar <manish.kumar11@genpact.com>
2019-06-13 12:12:44 +0530

    ios blur added

--- src/ios/PrivacyScreenBlur.m
+++ src/ios/PrivacyScreenBlur.m
@@ -3,27 +3,68 @@
 #import <Cordova/CDV.h>
 #import "AppDelegate.h"
 
-@interface PrivacyScreenBlur : CDVPlugin {
-  // Member variables go here.
-}
+// @interface PrivacyScreenBlur : CDVPlugin {
+//   // Member variables go here.
+// }
 
-- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+// - (void)coolMethod:(CDVInvokedUrlCommand*)command;
+// @end
+
+// @implementation PrivacyScreenBlur
+
+// - (void)coolMethod:(CDVInvokedUrlCommand*)command
+// {
+//     CDVPluginResult* pluginResult = nil;
+//     NSString* echo = [command.arguments objectAtIndex:0];
+
+//     if (echo != nil && [echo length] > 0) {
+//         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+//     } else {
+//         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+//     }
+
+//     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+// }
+
+// @end
+
+@interface AppDelegate (PrivacyScreenPlugin) {}
+
 @end
 
-@implementation PrivacyScreenBlur
+@implementation AppDelegate (PrivacyScreenPlugin)
 
-- (void)coolMethod:(CDVInvokedUrlCommand*)command
-{
-    CDVPluginResult* pluginResult = nil;
-    NSString* echo = [command.arguments objectAtIndex:0];
+- (void)applicationWillResignActive:(UIApplication *)application {
+    self.window.backgroundColor = [UIColor clearColor];
 
-    if (echo != nil && [echo length] > 0) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
+    UIBlurEffect *blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleRegular];
+    UIVisualEffectView *blurEffectView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
+    blurEffectView.frame = self.window.bounds;
+    blurEffectView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
 
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    blurEffectView.tag = 1234;
+    blurEffectView.alpha = 0;
+    [self.window addSubview:blurEffectView];
+    [self.window bringSubviewToFront:blurEffectView];
+
+    // fade in the view
+    [UIView animateWithDuration:0.5 animations:^{
+        blurEffectView.alpha = 1;
+    }];
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    NSLog(@"Hiding blur");
+    // grab a reference to our custom blur view
+    UIView *blurEffectView = [self.window viewWithTag:1234];
+
+    // fade away colour view from main view
+    [UIView animateWithDuration:0.2 animations:^{
+        blurEffectView.alpha = 0;
+    } completion:^(BOOL finished) {
+        // remove when finished fading
+        [blurEffectView removeFromSuperview];
+    }];
 }
 
 @end
